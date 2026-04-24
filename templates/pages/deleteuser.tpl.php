0cd71c22982e4127c1d143a4a1668ba14ddd369d
KocsisAgnesVirag <kocvirag@gmail.com>
2026-04-24 17:08:52 +0200

    crud 1.005
    a

--- templates/pages/deleteuser.tpl.php
+++ templates/pages/deleteuser.tpl.php
@@ -2,5 +2,5 @@
     include 'db_connect.php';
     $stmt = $connect->prepare('DELETE from munkaado where mhelyid = :mhelyid');
     $stmt->execute(array(':mhelyid' => $_GET["mhelyid"]));
-    header("Location: .");  // Go to the main folder
+    header("Location: ./templates/pages/");  // Go to the main folder
 ?>
